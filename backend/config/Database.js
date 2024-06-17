4e9236a51c8fdd92334f43a4c421b2217549db56
Syahrialll <trainer.be5.gamelab@gmail.com>
2024-06-17 15:12:11 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,8 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('Hgc4BBfbnFDOFrRk', 'NM8viuexHzD8vtxd', 'SV1qWPf0SrQyCE0n', {
+    host: "educalab.id",
+    port: 3307,
     dialect: "mysql"
 });
 
