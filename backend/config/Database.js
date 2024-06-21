b3168207019463ef958317ad3dfc724a2fafbf90
fareladrianp <fareladrian19@gmail.com>
2024-06-21 14:23:01 +0700

    Final

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('toko_online', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-})
+const db = new Sequelize(
+  "Hgc4BBfbnFDOFrRk",
+  "NM8viuexHzD8vtxd",
+  "SV1qWPf0SrQyCE0n",
+  {
+    host: "educalab.id",
+    port: 3307,
+    dialect: "mysql",
+  }
+);
 
 export default db;
